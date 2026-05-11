2cdc73441bc93e8e75f39e564299cddbb5b8c32f
al20250140008 <alf80169@gmail.com>
2026-05-11 11:43:49 +0700

    menambahkan kelas jantung dengan konstruktor dan destruktor

--- jantung.h
+++ jantung.h
@@ -2,3 +2,19 @@
 #include <string>
 using namespace std;
 
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
