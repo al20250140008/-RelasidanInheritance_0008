69c391c638014dbd552a167a14cf8f34eab441b3
al20250140008 <alf80169@gmail.com>
2026-05-11 11:45:12 +0700

    menghapus kode utama dan referensi kelas manusia serta jantung

--- komposisi.cpp
+++ komposisi.cpp
@@ -2,12 +2,3 @@
 #include <string>
 using namespace std;
 
-#include "jantung.h"
-#include "manusia.h"
-
-int main()
-{
-    manusia *varManusia = new manusia("Markus");
-    delete varManusia;
-    return 0;
-}
