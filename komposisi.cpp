0122e29f61275823978a38d29182d46745f26733
al20250140008 <alf80169@gmail.com>
2026-05-11 11:45:31 +0700

    menambahkan implementasi fungsi main untuk kelas manusia

--- komposisi.cpp
+++ komposisi.cpp
@@ -2,3 +2,12 @@
 #include <string>
 using namespace std;
 
+#include "jantung.h"
+#include "manusia.h"
+
+int main()
+{
+    manusia *varManusia = new manusia("Markus");
+    delete varManusia;
+    return 0;
+}
